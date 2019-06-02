4880d85187448d5a703ba91880ca5427f4727f4f
liudongjie <ldjwyyx@163.com>
2019-06-02 11:47:09 +0800

    set cookie

--- demos/hello/app.py
+++ demos/hello/app.py
@@ -5,16 +5,30 @@
 # @Site    : 
 # @File    : app.py
 # @Software: PyCharm
-
-from flask import Flask
+from flask import Flask, url_for, make_response, redirect
+import click
 
 app = Flask(__name__)
+# 配置变量
+app.config["SECRET_KEY"] = '4aa8-a690-ca241dfde751'
 
 
 @app.route('/hello', defaults={'name': 'world'})
 @app.route('/hello/<name>')
 def index(name):
-    return '<h1>Hello %s</h1>' % name
+    return '<h1>Hello %s</h1>' % url_for('index', name=name, _external=True)
+
+
+@app.cli.command()
+def hello():
+    click.echo('hello, human')
+
+
+@app.route('/302/<name>', methods=['GET', 'POST'])
+def reback(name):
+    resp = make_response(redirect(url_for('index', name=name)))
+    resp.set_cookie('name', name)
+    return resp
 
 
 if __name__ == '__main__':
