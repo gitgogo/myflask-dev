c296de9318d4516bf6af5ac9f4a4fcdcd1d0796e
liudongjie <ldjwyyx@163.com>
2019-06-03 23:27:30 +0800

    重定向地址安全校验，使用manager.run()

--- manager.py
+++ manager.py
@@ -7,10 +7,13 @@
 # @Software: PyCharm
 from flask import Flask, url_for, make_response, redirect, request, session, abort
 import click
+from urllib.parse import urlparse, urljoin
+from flask_script import Manager
 
 app = Flask(__name__)
 # 配置变量
 app.config["SECRET_KEY"] = '4aa8-a690-ca241dfde751'
+manager = Manager(app=app)
 
 
 @app.route('/hello', defaults={'name': 'world'})
@@ -84,10 +87,20 @@ def do_something():
 
 def redirect_back(default='index', **kwargs):
     for target in request.args.get('next'), request.referrer:
-        if target:
+        if not target:
+            continue
+        if is_safe_url(target):
             return redirect(target)
     return redirect(url_for(default, **kwargs))
 
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    print(ref_url, test_url)
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000, debug=True)
+    # app.run(host='0.0.0.0', port=9000, debug=True)
+    manager.run()
